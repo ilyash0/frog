72a02456b99cf485fe36d88740cd02f2fb91b12a
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2025-04-01 13:29:22 +0500

    Add base html & css

--- Frog/settings.py
+++ Frog/settings.py
@@ -16,8 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-AUTH_USER_MODEL = 'frog.User'
 AUTH_USER_MODEL = 'user.User'
+LOGIN_URL = '/login'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
