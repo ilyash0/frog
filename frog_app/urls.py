feb3297979d2e00042881aebd23ee31f06cfa0e7
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2025-05-12 13:19:20 +0500

    Add function to get frog

--- frog_app/urls.py
+++ frog_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, FrogsListView
+from .views import HomeView, FrogsListView, RandomFrogView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('frogs', FrogsListView.as_view(), name='frogs'),
+    path('get-frog', RandomFrogView.as_view(), name='get-frog'),
 ]
--- frog_app/views.py
+++ frog_app/views.py
@@ -1,6 +1,10 @@
 from django.views.generic import TemplateView, ListView, DetailView
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.db import transaction
+from datetime import date
 
-from frog_app.models import Frog
+from frog_app.models import Frog, FrogUser, FrogAcquisition
 
 
 class HomeView(TemplateView):
@@ -16,4 +20,39 @@ class FrogsDetailView(DetailView):
     model = Frog
     template_name = 'frog_app/frog_detail.html'
     ordering = ['rarity__id']
-# Create your views here.
+
+
+class RandomFrogView(TemplateView):
+    template_name = 'frog_app/home.html'
+
+    def post(self, request, *args, **kwargs):
+        if date.today().weekday() == 2:
+            already = FrogAcquisition.objects.filter(
+                user=request.user,
+                date=date.today()
+            ).exists()
+
+            if already:
+                messages.error(request, 'Вы уже получили сегодня свою лягушку.')
+                return redirect('home')
+
+            frog = Frog.objects.order_by('?').first()
+
+            if frog:
+                with transaction.atomic():
+                    frog_user, created = FrogUser.objects.get_or_create(
+                        user=request.user,
+                        frog=frog,
+                        defaults={'quantity': 1}
+                    )
+
+                    if not created:
+                        frog_user.quantity += 1
+                        frog_user.save()
+
+                    FrogAcquisition.objects.create(user=request.user, frog=frog)
+                    messages.success(request, f'Поздравляем! Вы получили лягушку «{frog.name}».')
+        else:
+            messages.error(request, 'Новые лягушки выдаются только по средам.')
+
+        return redirect('home')
